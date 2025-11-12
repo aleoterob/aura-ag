f6d5f65bfd644cf02a1c27153b6aee6dedd275a6
aleoterob <aleoterob@gmail.com>
2025-11-12 18:41:55 -0300

    feat: create auth-footer.tsx

---
+++ src/components/auth/auth-footer.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export function AuthFooter() {
+  return (
+    <footer className="w-full text-center text-xs text-muted-foreground">
+      &copy;{" "}
+      <Link
+        href="https://www.linkedin.com/in/aleoterob/"
+        target="_blank"
+        rel="noreferrer"
+        className="text-accent hover:text-primary transition-colors"
+      >
+        Alejandro Otero
+      </Link>{" "}
+      2025 | v0.0.1-beta
+    </footer>
+  );
+}
